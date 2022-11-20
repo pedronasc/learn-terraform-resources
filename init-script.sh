47fc2ff09a52aa3d56eb2f0a6ba30e092d34f8f4
Pedro Nascimento <pedronasc@engcomp.eng.br>
2022-11-20 00:57:52 -0300

    Estudos Final 20.11

--- init-script.sh
+++ init-script.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-yum update -y
-yum -y remove httpd
-yum -y remove httpd-tools
-yum install -y httpd24 php72 mysql57-server php72-mysqlnd
-service httpd start
-chkconfig httpd on
+sudo yum update -y
+sudo yum -y remove httpd
+sudo yum -y remove httpd-tools
+sudo yum install -y httpd php mysql php-mysqlnd
+sudo service httpd start
+sudo chkconfig httpd on
 
-usermod -a -G apache ec2-user
-chown -R ec2-user:apache /var/www
-chmod 2775 /var/www
-find /var/www -type d -exec chmod 2775 {} \;
-find /var/www -type f -exec chmod 0664 {} \;
+sudo usermod -a -G apache ec2-user
+sudo chown -R ec2-user:apache /var/www
+sudo chmod 2775 /var/www
+sudo find /var/www -type d -exec chmod 2775 {} \;
+sudo find /var/www -type f -exec chmod 0664 {} \;
 cd /var/www/html
 curl http://169.254.169.254/latest/meta-data/instance-id -o index.html
 curl https://raw.githubusercontent.com/hashicorp/learn-terramino/master/index.php -O
--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-west-2"
+  region = "us-east-1"
 }
 
 provider "random" {}
@@ -7,11 +7,18 @@ provider "random" {}
 resource "random_pet" "name" {}
 
 resource "aws_instance" "web" {
-  ami           = "ami-a0cfeed8"
+  ami           = "ami-0b0dcb5067f052a63"
   instance_type = "t2.micro"
   user_data     = file("init-script.sh")
+  key_name      = "my-key-pair2"
 
   tags = {
     Name = random_pet.name.id
   }
 }
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "my-key-pair2"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDUsg8HoD+juIPyPaw98QLQC8t7CONIYPLUIka9x8EhmkFxs9/v6Rce6zq16TYX/a7QMJWBTE2rtqH18C3E0pgQbQbc4g9VkJd8ywObNgzMLDmrcahlZfeDGag0RsMoKA5S9p1xRgRc/9aoxnl+L3+XQXhlUjt78FsGyq1Qgni+VkYkAeEUleGP93TGEFmFtwkqA40MM3J3J9ADzo11cAUjsE+PNrQiLxEwgedCuFvbEU8BXxLBLyA18lRZnW1Q4YwhK/xPoL5pqWE7ku9pFOuYM4UU1SFqPFdM9aui9yO2BtTvlU5QKtgEpwGEtzrtmmDXkmOX1mJ27TyyeDNmLZB1hHMmYa6xMBnTVvbyBBIXADDahyraPTcaA9vKL06v7x+5v3HvI2aXo5nJBrehPHen4fUytE3p6lIaKW9b9i2VZ4ScNO+W9gbhIPtpbT4fCA62wM4fvCCC1Cckb9QgFjWghLDwR6HlvNNnzLEX1jlQDcxc3lzfqrRkS9FEvSVL/ME="
+}
+
